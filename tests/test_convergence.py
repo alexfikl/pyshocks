de567127e0c0056a20365dd82926a29e8789df64
Alexandru Fikl <alexfikl@gmail.com>
2022-08-21 15:30:05 +0300

    enable disabled test cases

--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -379,12 +379,12 @@ class SATAdvectionTestCase(FiniteDifferenceTestCase):
 @pytest.mark.parametrize(
     ("case", "order", "resolutions"),
     [
-        # (AdvectionTestCase("godunov", "constant"), 1, list(range(80, 160 + 1, 16))),
-        # (AdvectionTestCase("godunov", "muscl"), 2, list(range(80, 160 + 1, 16))),
-        # (AdvectionTestCase("godunov", "wenojs32"), 3, list(range(192, 384 + 1, 32))),
-        # (AdvectionTestCase("godunov", "wenojs53"), 5, list(range(32, 256 + 1, 32))),
-        # (AdvectionTestCase("godunov", "esweno32"), 3, list(range(32, 256 + 1, 32))),
-        # (AdvectionTestCase("godunov", "ssweno242"), 4, list(range(192, 384 + 1, 32))),
+        (AdvectionTestCase("godunov", "constant"), 1, list(range(80, 160 + 1, 16))),
+        (AdvectionTestCase("godunov", "muscl"), 2, list(range(80, 160 + 1, 16))),
+        (AdvectionTestCase("godunov", "wenojs32"), 3, list(range(192, 384 + 1, 32))),
+        (AdvectionTestCase("godunov", "wenojs53"), 5, list(range(32, 256 + 1, 32))),
+        (AdvectionTestCase("godunov", "esweno32"), 3, list(range(32, 256 + 1, 32))),
+        (AdvectionTestCase("godunov", "ssweno242"), 4, list(range(192, 384 + 1, 32))),
         (SATAdvectionTestCase("sbp", "sbp21"), 2, list(range(80, 160 + 1, 16))),
         (SATAdvectionTestCase("sbp", "sbp42"), 3, list(range(192, 384 + 1, 32))),
     ],
