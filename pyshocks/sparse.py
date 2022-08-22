426d9b55eb13788dcc4982c618d66bb1e8a7132b
Alexandru Fikl <alexfikl@gmail.com>
2022-08-22 20:12:40 +0300

    sketch out a sparse module

---
+++ pyshocks/sparse.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2022 Alexandru Fikl <alexfikl@gmail.com>
+# SPDX-License-Identifier: MIT
+
+from typing import Optional
+
+import jax.numpy as jnp
+import jax.experimental.sparse as js
+
+
+def sparse_banded(qi: jnp.ndarray) -> js.BCOO:
+    raise NotImplementedError
+
+
+def sparse_banded_boundary(
+    qi: jnp.ndarray,
+    qb_l: Optional[jnp.ndarray],
+    qb_r: Optional[jnp.ndarray],
+) -> js.BCOO:
+    raise NotImplementedError
+
+
+def sparse_circulant(qi: jnp.ndarray) -> js.BCOO:
+    raise NotImplementedError
