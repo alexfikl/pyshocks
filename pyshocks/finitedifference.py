b8ef7aea7fc04c8531579d72bb8368723fc2a374
Alexandru Fikl <alexfikl@gmail.com>
2022-10-29 21:00:51 +0300

    add Fornberg finite difference method

--- pyshocks/finitedifference.py
+++ pyshocks/finitedifference.py
@@ -8,6 +8,7 @@ Finite Difference Approximations
 .. autoclass:: Stencil
 .. autofunction:: determine_stencil_truncation_error
 .. autofunction:: make_taylor_approximation
+.. autofunction:: make_fornberg_approximation
 """
 
 import math
@@ -107,6 +108,10 @@ def determine_stencil_truncation_error(
     return i - derivative, c
 
 
+def apply_stencil(d: Stencil, f: jnp.ndarray) -> jnp.ndarray:
+    pass
+
+
 # }}}
 
 
@@ -120,7 +125,9 @@ def make_taylor_approximation(
     atol: float = 1.0e-6,
     dtype: Optional["jnp.dtype[Any]"] = None,
 ) -> Stencil:
-    r"""
+    r"""Determine a finite difference stencil by solving a linear system from the
+    Taylor expansion.
+
     :arg derivative: integer order of the approximated derivative, e.g. ``1`` for
         the first derivative.
     :arg stencil: left and right bounds on the stencil around a point :math:`x_i`.
@@ -131,12 +138,13 @@ def make_taylor_approximation(
     """
     assert len(stencil) == 2
     assert stencil[0] < stencil[1]
+    assert stencil[0] < 0 or stencil[1] > 0
     assert derivative > 0
 
     if dtype is None:
         dtype = jnp.dtype(jnp.float64)
 
-    # set up
+    # setup
     indices = jnp.arange(stencil[0], stencil[1] + 1)
     A = jnp.array(
         [indices**i / math.factorial(i) for i in range(indices.size)], dtype=dtype
@@ -154,6 +162,86 @@ def make_taylor_approximation(
 
     return Stencil(
         derivative=derivative, order=order, coeffs=x, indices=indices, trunc=c
+    )
+
+
+# }}}
+
+
+# {{{ Fornberg approximation
+
+
+def make_fornberg_approximation(
+    derivative: int,
+    stencil: Tuple[int, int],
+    *,
+    atol: float = 1.0e-6,
+    dtype: Optional["jnp.dtype[Any]"] = None,
+) -> Stencil:
+    r"""Determine a finite difference stencil by Fornberg's method [Fornberg1998]_.
+
+    This method gives essentially the same results as :func:`make_taylor_approximation`
+    but uses an alternative method. This function is less vectorized and likely
+    to perform worse than the Taylor method if used in a loop.
+
+    .. [Fornberg1998] B. Fornberg, *Calculation of Weights in Finite
+        Difference Formulas*, SIAM Review, Vol. 40, pp. 685--691, 1998,
+        `DOI <http://dx.doi.org/10.1137/s0036144596322507>`__.
+
+    :arg derivative: integer order of the approximated derivative, e.g. ``1`` for
+        the first derivative.
+    :arg stencil: left and right bounds on the stencil around a point :math:`x_i`.
+        For example, ``(-1, 2)`` defines the 4 point stencil
+        :math:`\{x_{i - 1}, x_i, x_{i + 1}, x_{i + 2}\}`.
+
+    :arg atol: tolerance used in determining the order of the approximation.
+    """
+
+    assert len(stencil) == 2
+    assert stencil[0] < stencil[1]
+    assert stencil[0] < 0 or stencil[1] > 0
+    assert derivative > 0
+
+    if dtype is None:
+        dtype = jnp.dtype(jnp.float64)
+
+    # setup
+    x = jnp.arange(stencil[0], stencil[1] + 1, dtype=dtype)
+    xd = 0
+
+    # {{{ determine coefficients
+
+    c = jnp.zeros((x.size, derivative + 1), dtype=dtype)
+    c = c.at[0, 0].set(1.0)
+
+    c1, c4 = 1, x[0] - xd
+    for i in range(1, x.size):
+        j = jnp.arange(0, min(i, derivative) + 1)
+        c2, c5, c4 = 1, c4, x[i] - xd
+
+        for k in range(i):
+            c3 = x[i] - x[k]
+            c2, c6, c7 = c2 * c3, j * c[k, j - 1], c[k, j]
+            c = c.at[k, j].set((c4 * c7 - c6) / c3)
+
+        c = c.at[i, j].set(c1 * (c6 - c5 * c7) / c2)
+        c1 = c2
+
+    # only need the last derivative
+    c = c[:, -1]
+
+    # }}}
+
+    # determine order
+    # FIXME: we can probably figure this out without a while loop
+    order, trunc = determine_stencil_truncation_error(derivative, c, x)
+
+    return Stencil(
+        derivative=derivative,
+        order=order,
+        coeffs=c,
+        indices=x.astype(jnp.int64),
+        trunc=trunc,
     )
 
 
